a1b6eb04b9ac10775e5a0dd244a3ea2bc1194019
Ayushi Rajendra Kumar <54721860+Ayushi61@users.noreply.github.com>
2020-08-20 16:25:18 -0400

    Update game_of_life.f90
    needs updates

--- fortran/game_of_life.f90
+++ fortran/game_of_life.f90
@@ -1,22 +1,85 @@
 program main
+implicit none    
+    integer, parameter ::  rows = 5, columns = 5,gen=5
+    double precision,parameter::some=0.619
+    call sleep(1)
+    print *, "hello" 
+    call life(rows,columns,some,gen)
     
-    integer, parameter :: seed = 86456, rows = 10, columns = 5
-    double precision, parameter :: fraction = 0.3
-    integer :: initial(rows, columns), i, j 
-
-    call srand(seed)
-    do i=1,rows
-        do j=1,columns
-            if (rand()<fraction) then 
-                initial(i, j) = 1
-            else 
-                initial(i,j)=0
-            endif
-        end do
-    end do
-
-    DO i = 1, rows
-        PRINT '(1x, 5I6)', (initial(i,j), j = 1, columns)
-    END DO
-
 end program main
+
+
+subroutine life (rows,cols,some,gen)
+implicit none     
+    
+    integer,INTENT(IN) :: rows,cols
+    integer, INTENT(INOUT) :: gen
+    integer,DIMENSION(rows*cols) :: now
+    double precision,INTENT(IN)::some
+    integer :: i, j 
+    do i=1,rows*cols
+        !do j=1,cols
+            if (rand()<some) then 
+                now(i) = 1
+            else 
+                now(i)=0
+            endif
+        !end do
+    end do
+    do i = 1, rows*cols
+        if (modulo(i-1,rows)==0) then
+            write(*,*)
+        endif
+        write(*,"(1x, i0)",advance="no") now(i)
+    end do
+    write(*,*)
+    call live(now,rows,cols,gen)
+end subroutine life
+
+
+RECURSIVE subroutine live (a,r,c,gen)    
+implicit none   
+    integer,INTENT(IN):: r,c
+    integer, INTENT(INOUT) ::a(r*c)
+    integer, INTENT(INOUT) :: gen
+    integer:: i,nei,start1,end1,m
+    integer::b(1:SIZE(a,1))
+    call sleep(1)
+    write (*,"(A)") gen
+    start1=c+2
+    end1=(r*c)-c-2
+    b=a
+    do i = start1,end1
+        nei = a(i-1)+a(i+1)+a(i+c)+a(i-c)+a(i+c+1)+a(i+c-1)+a(i-c-1)+a(i-c-1)
+        b(i)=a(i)
+        if(a(i)==0) then
+            if(nei==3) then
+                b(i)=1
+            else
+                b(i)=0
+            endif
+        else
+            if(nei==2 .or. nei==3) then
+                b(i)=1
+            else
+                b(i)=0
+            endif
+        endif
+    end do
+    do i = 1, r*c
+        if (modulo(i-1,r)==0) then
+            write(*,*)
+        endif
+        write(*,"(1x, i0)",advance="no") b(i)
+    end do
+    write(*,*)
+    write(*,"(A)") "recursive"
+    !m = (gen-1)
+    !gen=m
+    a=b
+    if(gen .NE. 0) then
+        write(*,"(A, i0)") "recursive " gen
+        !call live(a,r,c,gen)
+    endif
+end subroutine live 
+
