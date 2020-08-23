ca8802261b701061b2b087c7a73c1a5db77e369c
Ayushi Rajendra Kumar <54721860+Ayushi61@users.noreply.github.com>
2020-08-22 22:03:05 -0400

    Update life_bug.f90

--- fortran/life_bug.f90
+++ fortran/life_bug.f90
@@ -5,8 +5,8 @@ implicit none
     integer, parameter ::  rows = 3, columns = 3,gen=5
     integer, parameter ::  rows1 = 3, columns1 = 3,gen1=4
     double precision,parameter::some=0.619
-    integer,DIMENSION(rows*columns)::now
-    integer,DIMENSION(rows1*columns1)::now1,final
+    integer,DIMENSION(rows*columns)::now,outnow
+    integer,DIMENSION(rows1*columns1)::now1,outnow1,final
     integer :: i
     call GET_COMMAND_ARGUMENT(1,choice)
     if (choice .EQ. 'a') then
@@ -19,39 +19,43 @@ implicit none
         end do
 		call sleep(1)
         call cls()
-        call life(now,rows,columns,gen)
+        call hm()
+        call life(now,rows,columns,gen,outnow)
     else
         now1=(/0,0,0,1,1,1,0,0,0/)
         call sleep(1)
         call cls()
-        call life(now1,rows1,columns1,gen1)
+        call hm()
+        call life(now1,rows1,columns1,gen1,outnow1)
     endif
-    call sleep(1)
-    call hm()
-    call cls()
-    !if (choice .NE. 'a') then
-    !   final = (/0,0,0,0,0,0,0,0,0/)
-    !    equal = size(now) == size(final)
-    !    if (equal) then
-    !        do i=1,size(now)
-    !            equal = equal .and. now(i) == final(i) 
-    !            print *, equal, now(i)
-    !            if ( .not. equal )exit
-    !        enddo
-    !    endif
-    !    if(equal) then
-    !        print *, "All good!!"
-    !    endif
-    !endif
+    if (choice .NE. 'a') then
+       final = (/0,0,0,0,0,0,0,0,0/)
+        equal = size(outnow1) == size(final)
+        if (equal) then
+            do i=1,size(outnow1)
+                equal = equal .and. final(i) == outnow1(i) 
+                if ( .not. equal ) then
+                    print *, equal,final(i) ,outnow1(i)
+                    exit
+                endif
+            enddo
+        endif
+        if(equal) then
+            print *, "All good!!"
+        else
+            print *,"Not good!!"
+        endif
+    endif
 end program main
 
 
-subroutine life (now,rows,cols,gen)
+subroutine life (now,rows,cols,gen,outnow)
 implicit none     
     
     integer,INTENT(IN) :: rows,cols
     integer, INTENT(INOUT) :: gen
     integer,INTENT(INOUT) :: now(rows*cols)
+    integer,INTENT(OUT) :: outnow(rows*cols)
     integer :: i, j 
     integer::d(1:SIZE(now,1))
     d=now
@@ -67,15 +71,19 @@ implicit none
         endif
     end do
     write(*,*)
+    call sleep(1)
+    call cls()
+    call hm()
+    call live(now,rows,cols,gen,outnow)
     
-    call live(now,rows,cols,gen)
 end subroutine life
 
 
-RECURSIVE subroutine live (a,r,c,gen)    
+RECURSIVE subroutine live (a,r,c,gen,outnow)    
 implicit none   
     integer,INTENT(IN):: r,c
     integer, INTENT(INOUT) ::a(r*c)
+    integer, INTENT(OUT) ::outnow(r*c)
     integer, INTENT(INOUT) :: gen
     integer:: i,nei,m
     integer::b(1:SIZE(a,1))
@@ -117,8 +125,6 @@ implicit none
         endif
     end do
     m = (gen-1)
-	call sleep(1)
-    call hm()
     write(*,*) "recursive ", m
     do i = 1, r*c
         if (modulo(i-1,r)==0) then
@@ -132,7 +138,11 @@ implicit none
     end do
     write(*,*)
     if(m .NE. 0) then
-        call live(b,r,c,m)
+        call sleep(1)
+        call cls()
+        call hm()
+        outnow=b
+        call live(b,r,c,m,outnow)
     endif
 end subroutine live 
 
